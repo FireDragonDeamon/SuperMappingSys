7ca0817deccd8f71d0eb9ec728ec79252ac73c5e
SHARMA <sss919482@tatatechnologies.com>
2023-04-11 10:52:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ctb-model-map';
+  constructor(){
+    setTimeout(() => {
+      this.title = "Chnaged" 
+    }, 2000);
+  }
 }
